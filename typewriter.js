2eaf4144086e72e457c7aabc82525ca33b29ade9
Yamac Ozdil <yamac.ozdil@outlook.com>
2021-02-09 12:41:58 +0000

    typewriter function

--- typewriter.js
+++ typewriter.js
@@ -1,12 +1,15 @@
 const sentence = "hello there from lighthouse labs";
-const y = sentence.length;
-let x = 0;
-setTimeout(() => {
-  for (const char of sentence) {
-    process.stdout.write(char);
-    x += 50;
-  }
+const newLineTime = sentence.length;
+
+let time = 0;
+
+for (const char of sentence) {
   setTimeout(() => {
-    console.log("");
-  }, 50 * y);
-}, x);
+    process.stdout.write(char);
+  }, time);
+  time += 50;
+}
+
+setTimeout(() => {
+  console.log("");
+}, 50 * newLineTime);
